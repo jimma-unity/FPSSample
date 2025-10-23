2eeb280a0e2ca1687a7d819cd52510b6f598a7f6
jamespa <jamespa@unity3d.com>
2025-10-23 11:08:56 +0200

    Fix missing engine types when migrating to Entties 1.4.x

--- Assets/Scripts/ECS/ComponentRegistration.cs
+++ Assets/Scripts/ECS/ComponentRegistration.cs
@@ -7,3 +7,18 @@
 [assembly: RegisterGenericComponentType(typeof(InitializeComponentSystem<RagdollOwner>.SystemState))]
 [assembly: RegisterGenericComponentType(typeof(InitializeComponentSystem<NamePlateOwner>.SystemState))]
 [assembly: RegisterGenericComponentType(typeof(InitializeComponentSystem<PlayerCameraSettings>.SystemState))]
+
+// JAPA ¯\_(ツ)_/¯ - These would ideally ultimately be removed (or at least partially) as it indicates
+// we are accessing types from ECS code that we ideally should not.
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.Animator))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.AudioListener))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.AudioReverbFilter))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.Camera))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.Canvas))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.FlareLayer))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.MeshFilter))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.Rendering.HighDefinition.HDAdditionalCameraData))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.Rendering.PostProcessing.PostProcessLayer))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.RectTransform))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.UI.CanvasScaler))]
+[assembly: RegisterGenericComponentType(typeof(UnityEngine.UI.GraphicRaycaster))]
