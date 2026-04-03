0ec4e7cbd6a3e540dd01981c07117137a2427a37
jamespa <jamespa@unity3d.com>
2026-04-03 13:07:45 +0200

    Fix bad formatting

--- Assets/Scripts/Utils/Timeline/CommandClip.cs
+++ Assets/Scripts/Utils/Timeline/CommandClip.cs
@@ -16,5 +16,6 @@ public class CommandClip : PlayableAsset, ITimelineClipAsset
     public override Playable CreatePlayable (PlayableGraph graph, GameObject owner)
     {
         var playable = ScriptPlayable<CommandBehaviour>.Create (graph, template);
-        return playable;    }
+        return playable;
+    }
 }
